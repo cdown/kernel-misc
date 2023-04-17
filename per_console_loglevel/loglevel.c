fb9bb669a5b021278bae8e2ff17c8b6ba9b5e5c0
Chris Down <chris@chrisdown.name>
2023-04-17 13:45:17 +0100

    loglevel: Put out in struct

--- per_console_loglevel/loglevel.c
+++ per_console_loglevel/loglevel.c
@@ -15,38 +15,55 @@ struct console_cmdline {
 	short flags;
 };
 
-static int parse_console_cmdline_options(struct console_cmdline *c,
-					 char *options)
-{
-	char *item;
-	bool seen_serial_opts = false;
+struct console_option {
+	char *key;
+	char *value;
+};
 
-	while ((item = strsep(&options, ",")) != NULL) {
+static struct console_option find_and_remove_console_option(char *wanted,
+							    char *options)
+{
+	struct console_option ret = { NULL, NULL };
+	bool found = false, first = true;
+	char *item, *opt = options;
+
+	while ((item = strsep(&opt, ",")) != NULL) {
 		char *key = item, *value;
 
 		value = strchr(item, ':');
 		if (value)
 			*(value++) = '\0';
 
-		if (strcmp(key, "loglevel") == 0 && value &&
-		    isdigit(value[0]) && strlen(value) == 1) {
-			c->level = clamp(value[0] - '0', LOGLEVEL_EMERG,
-					 LOGLEVEL_DEBUG + 1);
-			c->flags |= CON_LOGLEVEL;
-			continue;
+		if (strcmp(key, wanted) == 0) {
+			found = true;
+			ret.key = strdup(key);
+			if (value)
+				ret.value = strdup(value);
 		}
 
-		if (!seen_serial_opts && isdigit(key[0]) && !value) {
-			seen_serial_opts = true;
-			c->options = key;
-			continue;
-		}
+		if (opt)
+			*(opt - 1) = ',';
+		if (!first)
+			*(item - 1) = ',';
+		if (value)
+			*(value - 1) = ':';
 
-		printf("ignoring invalid console option: '%s%s%s'\n", key,
-		       value ? ":" : "", value ?: "");
+		if (found)
+			break;
+
+		first = false;
 	}
 
-	return 0;
+	if (found) {
+		if (opt)
+			memmove(item, opt, strlen(opt) + 1);
+		else if (first)
+			*item = '\0';
+		else
+			*--item = '\0';
+	}
+
+	return ret;
 }
 
 int main(int argc, char *argv[])
@@ -55,6 +72,7 @@ int main(int argc, char *argv[])
 	FILE *fp = NULL;
 	long size;
 	char *cmdline;
+	struct console_option level = { NULL, NULL };
 
 	if (argc == 2)
 		cmdline = argv[1];
@@ -84,9 +102,18 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	parse_console_cmdline_options(&con, cmdline);
-	printf("options: %s\n", con.options);
-	printf("level: %d\n", con.level);
+	printf("After removing loglevel:\n");
+	level = find_and_remove_console_option("loglevel", cmdline);
+	con.options = cmdline;
+	printf("options passed to driver: %s\n", con.options);
+	printf("command line options: %s\n", cmdline);
+	if (level.key)
+		printf("level: key: %s, value: %s\n", level.key, level.value);
+
+	if (level.key)
+		free(level.key);
+	if (level.value)
+		free(level.value);
 
 	if (fp) {
 		fclose(fp);
